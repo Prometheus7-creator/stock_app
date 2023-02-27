9183c617b6b3c12d7b3af092b4e19b78b79e3401
Ansh Grover <ansh111666@gmail.com>
2023-02-27 17:24:33 +0530

    Added barchart for search results

--- src/pages/ResultPage.js
+++ src/pages/ResultPage.js
@@ -1,10 +1,18 @@
 import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
+import BarChart from "../components/BarChart";
 import NavBar from "../components/Navbar";
 
 const ResultPage = () =>{
     const [searchParams, setSearchParams] = useSearchParams();
     const [data, setData] = useState({});
+    const [userData, setUserData] = useState({
+        labels: ["Open", "High", "Low", "Price"],
+        datasets: [{
+            label: "Loss/Gain",
+            data: []
+        }]
+    });
 
     useEffect(()=>{
         const symbol = searchParams.get('name');
@@ -14,6 +22,17 @@ const ResultPage = () =>{
         .then(response => {
             response = response['Global Quote'];
             setData(response);
+            console.log(response)
+            setUserData({
+                labels: ["Open", "High", "Low", "Price"],
+                datasets: [{
+                    label: "Loss/Gain",
+                    data: [response['02. open'], response['03. high'], response['04. low'], response['05. price']],
+                    backgroundColor: ["rgba(75, 192, 192, 1)",
+                        "#f3ba2f"
+                    ]
+                }]
+            })
         })
         .catch(err => console.error(err));
 
@@ -22,11 +41,8 @@ const ResultPage = () =>{
     return (<div className="result-container">
         <NavBar/>
         <h2 style={{marginTop: '4rem'}}>{("01. symbol" in data)?data['01. symbol']: null}</h2>
-        <div className="result-box">
-        {Object.keys(data).map(key=><div className="result-row">
-            <div>{key}</div>
-            <div>{data[key]}</div>
-        </div>)}
+        <div style={{width: '80%', height: '50%', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+        <BarChart chartData={userData}/>
         </div>
     </div>)
 }
