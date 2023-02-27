a25fe42b0087801943593b347e2ce51a4c23ea81
Ansh Grover <ansh111666@gmail.com>
2023-02-28 00:09:53 +0530

    changed stock news api for news with banner images

--- src/components/NewsFeed.js
+++ src/components/NewsFeed.js
@@ -13,18 +13,16 @@ const NewsFeed = () =>{
     const feedcontainer = useRef();
 
     const loadData = () =>{
-        const options = {
-            method: 'GET',
-            headers: {
-                'X-RapidAPI-Key': '189a1b6639msheebed2040ce3668p15e63bjsn9c53a08c4837',
-                'X-RapidAPI-Host': 'yahoo-finance15.p.rapidapi.com'
-            }
-        };
+        const year = new Date().toLocaleDateString('en', {year: 'numeric'});
+        const month = new Date().toLocaleDateString('en', {month: '2-digit'});
+        const day = parseInt(new Date().toLocaleDateString('en', {day: '2-digit'}))-1;
+        // console.log(year + month + day-10);
 
-        fetch('https://yahoo-finance15.p.rapidapi.com/api/yahoo/ne/news', options)
+        fetch(`https://www.alphavantage.co/query?function=NEWS_SENTIMENT&time_from=${year+month+day}T0000&limit=100&apikey=JAARZWOLNUOZ6AI6`, {method: 'GET'})
         .then(response => response.json())
         .then(response => {
-            setNewsData(response);
+            console.log(response['feed']);
+            setNewsData(response['feed']);
             console.log(newsData)    
         })
     }
@@ -38,8 +36,9 @@ const NewsFeed = () =>{
 
 
     const getDate = (dateStr) =>{
-        const date = new Date(dateStr);
-        return date.toLocaleDateString("en-US", { year: 'numeric', month: 'long', day: 'numeric' })
+        const date = dateStr.split('T')[0];
+        const d = new Date(`${date.slice(0,4)}-${date.slice(4,6)}-${date.slice(6,8)}`);
+        return d.toLocaleDateString("en-US", { year: 'numeric', month: 'long', day: 'numeric' })
     }
 
     const startScroll = () => {
@@ -66,15 +65,15 @@ const NewsFeed = () =>{
                 newsData.map((news, index) => <div key={index} className="news-card">
                     <figure>
                         <div className="date">
-                            {getDate(news.pubDate)}
+                            {getDate(news.time_published)}
                         </div>
-                        <img src={screen} alt={news.title}/>
+                        <img src={news.banner_image || screen} alt={news.title}/>
 
                         <div className="caption">
                         <h2>{news.source}</h2>
                         <p>{news.title}</p>
                         </div>
-                        <a href={news.link} target="_blank" rel="noreferrer" className="read-more">Read More</a>
+                        <a href={news.url} target="_blank" rel="noreferrer" className="read-more">Read More</a>
                     </figure>
                 </div>)
             }
